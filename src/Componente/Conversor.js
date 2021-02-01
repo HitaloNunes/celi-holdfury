f5925c53a0abb5489af64e9cbdd8b237ceb86fb7
HITALO WINGRED ALVES NUNES <hitalowingred@hotmail.com>
2021-02-01 15:00:10 -0300

    Primeiro Commit

---
+++ src/Componente/Conversor.js
@@ -0,0 +1,43 @@
+import { Component } from "react";
+import './Conversor.css'
+
+export default class Conversor extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            moedaA_valor:"",
+            moedaB_valor:0,
+        }
+
+        this.converter = this.converter.bind(this);
+    }
+
+    converter(){
+        let de_para = `${this.props.moedaA}_${this.props.moedaB}`;
+        let url = `https://free.currconv.com/api/v7/convert?apiKey=70e970fc980347b6b436&q=${de_para}&compact=y`;
+
+        fetch(url)
+        .then(res=>{
+            return res.json();
+        })
+        .then(json=>{
+            let cotacao = json[de_para].val;
+            let moedaB_valor = ( this.state.moedaA_valor * cotacao).toFixed(2)
+            this.setState({moedaB_valor});
+        });
+        //console.log(url);
+    }
+
+    render() {
+        return (
+            <div className="conversor">
+                <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
+                <input type="text" onChange={(event)=>{this.setState({moedaA_valor:event.target.value})}}></input>
+                <input type="button" value="Converter" onClick={this.converter}></input>
+                <h2>{this.state.moedaB_valor}</h2>
+            </div>
+        )
+    }
+}
